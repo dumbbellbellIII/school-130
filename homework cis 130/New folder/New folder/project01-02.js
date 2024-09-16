ce33d4bee4cbf74e8834cd9090bdc587e345950c
dumbbellbellIII <143977093+dumbbellbellIII@users.noreply.github.com>
2024-09-16 19:01:00 -0400

    Add files via upload

---
+++ homework cis 130/New folder/New folder/project01-02.js
@@ -0,0 +1,11 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: Meekaaeel Abdullah
+    Date:   9/15/24
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
---
+++ homework cis 130/project02/project01-02.js
@@ -0,0 +1,42 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: Meekaaeel Abdullah
+    Date:   9/16/2024
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
+let service1Name = "Basic", service2Name = "Express", 
+    service3Name = "Ultimate",
+    service4Name = "Extreme";
+let service1speed = "0 Mbps", service2Speed = "100 Mbps", service3Speed = "100 Mbps",
+    service4Speed = "1 Gig";
+
+
+<><td>
+    <script>
+        document.write (service1Name ="basic");
+        document.write (service2Name ="Express");
+        document.write (service3Name ="Extreme");
+        document.write (service4Name ="Ultimate");
+    </script>
+</td>
+<td>
+     <script>
+            document.write (service1Speed ="0 Mbps");
+            document.write (service2Speed ="100 Mbps");
+            document.write (service3Speed ="500 Mbps");
+            document.write (servce4speed ="1 Gig");
+
+     </script>
+</td> 
+         <td>
+            <script>
+              
+            </script>
+         </td>
+        
+    
